f7c803b83aea43ceafa604c183b061149bc303c1
xuxinlai2002 <xxl@local.com>
2025-04-14 23:53:27 +0800

    fix the rpc call probrem

--- testnet/scripts/start_local.sh
+++ testnet/scripts/start_local.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# 检查是否已经初始化
+# 检查是否已初始化
 if [ ! -d "../.sei" ]; then
-    echo "节点尚未初始化,正在执行初始化..."
+    echo "节点未初始化，正在初始化..."
     ./init_local.sh
 fi
 
-# 启动节点
 echo "正在启动节点..."
-seid start --home ../.sei \
-  --rpc.laddr tcp://0.0.0.0:26657 \
-  --grpc.address 0.0.0.0:9090 \
-  --address tcp://0.0.0.0:26656 \
-  --minimum-gas-prices 0.0001usei \
-  --p2p.pex=false \
-  --p2p.persistent-peers="" \
-  --mode validator 
+seid start \
+    --home ../.sei \
+    --rpc.laddr tcp://0.0.0.0:26657 \
+    --p2p.laddr tcp://0.0.0.0:26656 \
+    --grpc.address 0.0.0.0:9090 \
+    --grpc-web.address 0.0.0.0:9091 \
+    --minimum-gas-prices 0.0001usei \
+    --p2p.pex=false \
+    --p2p.persistent-peers="" \
+    --mode validator 
